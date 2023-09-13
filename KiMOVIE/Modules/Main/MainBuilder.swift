e1864d4f77a28019d97bb393e4c36a3655320f8b
Kirill Drozdov <kirilldrozd12@gmail.com>
2023-09-13 13:03:27 +0300

    added builder first view

--- KiMOVIE/Modules/Main/MainBuilder.swift
+++ KiMOVIE/Modules/Main/MainBuilder.swift
@@ -8,5 +8,22 @@
 import UIKit
 
 class MainBuilder {
-	
+	static func viewController() -> MainViewController {
+		let viewController = MainViewController()
+		let interactor = MainInteractor()
+		let presenter = MainPresenter()
+		let router = MainRouter()
+
+		viewController.interactor = interactor
+		viewController.router = router
+
+		interactor.presenter = presenter
+
+		presenter.viewController = viewController
+
+		router.viewController = viewController
+		router.dataStore = interactor
+
+		return viewController
+	}
 }
